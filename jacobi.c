872faf21f1b979225f62d08b554c8b5855a6e3e7
jasminevesto <jasmine.truchot@gmail.com>
2022-01-11 17:17:27 +0100

    Jacobi method

--- jacobi.c
+++ jacobi.c
@@ -3,7 +3,33 @@
  */
 #include <math.h>
 
-void
-jacobi() {
-    // fill in your code here
+// N is the number of points per dimension
+void jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, double threshold)
+{
+    int i, j, k, num_iter;
+    double delta = 2 / N;
+
+    float distance = INFINITY;
+
+    double s = 1 / 6;
+    while (distance < threshold && num_iter < iter_max)
+    {
+        // Put the distance between u and u_upd to 0 now that the loop is started
+        distance = 0;
+        for (i = 1; i < N + 1; i++)
+            for (j = 1; j < N + 1; j++)
+                for (k = 1; k < N + 1; k++)
+                {
+                    u_upd[i][j][k] = u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k];
+                    u_upd[i][j][k] *= s;
+
+                    // Update the distance between u and u_upd
+                    distance += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
+                }
+
+        // Put u_upd into u to remake the calculations another time
+        u = u_upd;
+
+        num_iter += 1;
+    }
 }
