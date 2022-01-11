0578b75fce2f836709c3ad4a0f23655ddd3c3d06
gsp1405 <spalj61@gmail.com>
2022-01-11 16:59:58 +0100

    Added allocation and initialization in main

--- main.c
+++ main.c
@@ -19,7 +19,7 @@
 int
 main(int argc, char *argv[]) {
 
-    int 	N = N_DEFAULT;
+    int 	N = N_DEFAULT, M;
     int 	iter_max = 1000;
     double	tolerance;
     double	start_T;
@@ -27,7 +27,8 @@ main(int argc, char *argv[]) {
     char	*output_prefix = "poisson_res";
     char        *output_ext    = "";
     char	output_filename[FILENAME_MAX];
-    double 	***u = NULL;
+    double 	***u = NULL, ***f = NULL, ***u_upd = NULL;
+    int i, j, k;
 
 
     /* get the paramters from the command line */
@@ -36,21 +37,59 @@ main(int argc, char *argv[]) {
     tolerance = atof(argv[3]);  // tolerance
     start_T   = atof(argv[4]);  // start T for all inner grid points
     if (argc == 6) {
-	output_type = atoi(argv[5]);  // ouput type
+	output_type = atoi(argv[5]);  // output type
     }
-
-    // allocate memory
-    if ( (u = d_malloc_3d(N, N, N)) == NULL ) {
+    M = N + 2;
+    // allocate memory for u, u_upd and f
+    if ( (u = d_malloc_3d(M, M, M)) == NULL ) {
         perror("array u: allocation failed");
         exit(-1);
     }
+    if ( (u_upd = d_malloc_3d(M, M, M)) == NULL ) {
+        perror("array u_upd: allocation failed");
+        exit(-1);
+    }
+    if ( (f = d_malloc_3d(M, M, M)) == NULL ) {
+        perror("array f: allocation failed");
+        exit(-1);
+    }
+    
 
-    /*
-     *
-     * fill in your code here 
-     *
-     *
-     */
+    //Set initial values for u
+    for(i = 0; i < M; i++){
+        for(j = 0; j < M; j++){
+            for(k = 0; k < M; k++){
+                if ((i == 0) || (i == M) || (j == M) || (k == 0) || (k == M)){
+                    u[i][j][k] = 20;
+                }
+                else if (j == 0){
+                    u[i][j][k] = 0;
+                }
+                else{
+                    u[i][j][k] = start_T;
+                }
+
+            }
+        }
+    }
+
+    //Set values for f
+    for(i = 0; i < M; i++){
+        for(j = 0; j < M; j++){
+            for(k = 0; k < M; k++){
+                if ((i <= M * 3 / 16) && (j <= M / 4) && (k <= M / 2) && (k >= M * 2 / 16)){
+                    f[i][j][k] = 200;
+                }
+                else{
+                    f[i][j][k] = 0;
+                }
+
+            }
+        }
+    }
+
+    //Obtain solution u using the Jacobi method (not sure if N or M, depends on implementation by Jasmine)
+    jacobi(u, u_upd, f, N, iter_max, tolerance);
 
     // dump  results if wanted 
     switch(output_type) {
