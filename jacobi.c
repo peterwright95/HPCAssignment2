d564d7cc353eec42d8ec16cfcc4f30ebfb64a4a6
Weijun-H <huangweijun1001@gmail.com>
2022-01-12 23:28:53 +0100

    change jacobi return type

--- jacobi.c
+++ jacobi.c
@@ -3,9 +3,10 @@
  */
 #include <math.h>
 #include <stdio.h>
+#include <omp.h>
 
 // N is the number of points per dimension
-void jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, double threshold)
+int jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, double threshold)
 {
     int i, j, k, num_iter = 0;
     double delta = 2 / (double)(N + 1);
@@ -15,7 +16,6 @@ void jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, doub
     float distance = INFINITY;
 
     double s = 1.0 / 6.0;
-    printf("S = %f\n", s);
     while (distance > threshold && num_iter < iter_max)
     {
         // Put the distance between u and u_upd to 0 now that the loop is started
@@ -42,9 +42,53 @@ void jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, doub
                 {
                     u[i][j][k] = u_upd[i][j][k];
                 }
-        if (num_iter < 10){
-            printf("Iteration : %d\nDistance is: %f\n\n",num_iter,distance);
-        }
+
         num_iter += 1;
     }
+    return num_iter;
+}
+
+
+int jacobi_baseline(double ***u, double ***u_upd, double ***f, int N, int iter_max, double threshold)
+{
+    int i, j, k, num_iter = 0;
+    double delta = 2 / (double)(N + 1);
+    double *swap;
+    printf("Delta is %f\n", delta);
+
+    float distance = INFINITY;
+
+    double s = 1.0 / 6.0;
+    printf("S = %f\n", s);
+    while (distance > threshold && num_iter < iter_max)
+    {
+        // Put the distance between u and u_upd to 0 now that the loop is started
+        distance = 0;
+        #pragma omp parallel for
+        for (i = 1; i < N + 1; i++)
+        {
+            for (j = 1; j < N + 1; j++)
+            {
+                for (k = 1; k < N + 1; k++)
+                {
+                    u_upd[i][j][k] = u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k];
+                    u_upd[i][j][k] *= s;
+
+                    // Update the distance between u and u_upd
+                    distance += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
+                }
+            }
+        }
+
+        // Put u_upd into u to remake the calculations another time
+        for (i = 1; i < N + 1; i++)
+            for (j = 1; j < N + 1; j++)
+                for (k = 1; k < N + 1; k++)
+                {
+                    u[i][j][k] = u_upd[i][j][k];
+                }
+
+        num_iter += 1;
+    }
+    return num_iter;
 }
