9a708827b2768ff4a70a8b23352baa93a260017e
Weijun-H <huangweijun1001@gmail.com>
2022-01-12 17:20:32 +0100

    fix iteration bug

--- jacobi.c
+++ jacobi.c
@@ -14,13 +14,16 @@ void jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, doub
 
     float distance = INFINITY;
 
-    double s = 1 / 6;
+    double s = 1.0 / 6.0;
+    printf("S = %f\n", s);
     while (distance > threshold && num_iter < iter_max)
     {
         // Put the distance between u and u_upd to 0 now that the loop is started
         distance = 0;
         for (i = 1; i < N + 1; i++)
+        {
             for (j = 1; j < N + 1; j++)
+            {
                 for (k = 1; k < N + 1; k++)
                 {
                     u_upd[i][j][k] = u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k];
@@ -29,6 +32,8 @@ void jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, doub
                     // Update the distance between u and u_upd
                     distance += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
                 }
+            }
+        }
 
         // Put u_upd into u to remake the calculations another time
         for (i = 1; i < N + 1; i++)
@@ -38,7 +43,7 @@ void jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, doub
                     u[i][j][k] = u_upd[i][j][k];
                 }
         if (num_iter < 10){
-            printf("Distance is: %f\n", distance);
+            printf("Iteration : %d\nDistance is: %f\n\n",num_iter,distance);
         }
         num_iter += 1;
     }
