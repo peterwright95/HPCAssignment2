4f6c997e7d08a9355521e5aea433c6b28a1d1ef9
Weijun-H <huangweijun1001@gmail.com>
2022-01-12 23:44:36 +0100

    create sh for test sertial version

---
+++ run_j.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# 02614 - High-Performance Computing, January 2022
+# 
+# batch script to run matmult on a decidated server in the hpcintro
+# queue
+#
+# Author: Group 14
+#
+#BSUB -J jacobi
+#BSUB -o mm_batch_%J.out
+#BSUB -q hpcintro
+#BSUB -n 1
+#BSUB -R "rusage[mem=2048]"
+#BSUB -W 00:15
+# uncomment the following line, if you want to assure that your job has
+# a whole CPU for itself (shared L3 cache)
+# #BSUB -R "span[hosts=1] affinity[socket(1)]"
+# choose the server to collect
+#BSUB -R "select[model == XeonE5_2650v4]"
+
+module load gcc
+
+# output cpu info
+lscpu
+
+EXECUTABLE=./poisson_j
+OUTFILE=jacobi_serial.dat
+
+# define the grid size here
+SIZES="10 20 50 100 200 500"
+
+# define the iteration time
+ITER="10000"
+
+# define the tolerance bound
+TOL="0.01"
+
+# define the start T
+START="1"
+
+
+# start the collect command with the above settings
+
+for S in $SIZES
+do
+    $EXECUTABLE $S $ITER $TOL $START $> DATAFILE/$OUTFILE
+done
+
+
+
