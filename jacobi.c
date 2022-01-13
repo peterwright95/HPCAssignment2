73e8eeb0ca8c6634b225024f55eb224c4dbbe4cf
Weijun-H <huangweijun1001@gmail.com>
2022-01-13 18:31:52 +0100

    update jacobi parallel

--- jacobi.c
+++ jacobi.c
@@ -49,26 +49,26 @@ int jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, doubl
 
 int jacobi_baseline(double ***u, double ***u_upd, double ***f, int N, int iter_max, double threshold)
 {
-    int i, j, k, num_iter = 0;
+    int i, j, k, num_iter = 0, threads;
     double delta = 2 / (double)(N + 1);
 
     float distance = INFINITY;
 
+    threads = omp_get_num_threads();
+
     double s = 1.0 / 6.0;
-    printf("S = %f\n", s);
     while (distance > threshold && num_iter < iter_max)
     {
         // Put the distance between u and u_upd to 0 now that the loop is started
         distance = 0;
-        #pragma omp parallel for
         for (i = 1; i < N + 1; i++)
         {
+            #pragma omp parallel for reduction(+: distance)
             for (j = 1; j < N + 1; j++)
             {
                 for (k = 1; k < N + 1; k++)
                 {
-                    u_upd[i][j][k] = u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k];
-                    u_upd[i][j][k] *= s;
+                    u_upd[i][j][k] = s*(u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k]);
 
                     // Update the distance between u and u_upd
                     distance += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
@@ -86,5 +86,51 @@ int jacobi_baseline(double ***u, double ***u_upd, double ***f, int N, int iter_m
 
         num_iter += 1;
     }
-    return num_iter;
+    printf("%d %f %d %d", N, distance, num_iter, threads);
+    return threads;
+}
+
+
+int jacobi_collapse(double ***u, double ***u_upd, double ***f, int N, int iter_max, double threshold)
+{
+    int i, j, k, num_iter = 0, threads;
+    double delta = 2 / (double)(N + 1);
+
+    float distance = INFINITY;
+
+    threads = omp_get_num_threads();
+
+    double s = 1.0 / 6.0;
+    while (distance > threshold && num_iter < iter_max)
+    {
+        // Put the distance between u and u_upd to 0 now that the loop is started
+        distance = 0;
+    		#pragma omp parallel shared(delta, u, u_upd, N, distance) private(i, j, k)
+        for (i = 1; i < N + 1; i++)
+        {
+            #pragma omp for reduction(+: distance)
+            for (j = 1; j < N + 1; j++)
+            {
+                for (k = 1; k < N + 1; k++)
+                {
+                    u_upd[i][j][k] = s*(u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k]);
+
+                    // Update the distance between u and u_upd
+                    distance += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
+                }
+            }
+        }
+
+        // Put u_upd into u to remake the calculations another time
+        for (i = 1; i < N + 1; i++)
+            for (j = 1; j < N + 1; j++)
+                for (k = 1; k < N + 1; k++)
+                {
+                    u[i][j][k] = u_upd[i][j][k];
+                }
+
+        num_iter += 1;
+    }
+    printf("%d %f %d %d", N, distance, num_iter, threads);
+    return threads;
 }
