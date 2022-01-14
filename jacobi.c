1b2f36f891b1e67b43ad074ff9b80dbd31a814ff
jasmine <jasmine.truchot@gmail.com>
2022-01-14 23:25:25 +0100

    Add Gauss-Seidel parallel

--- jacobi.c
+++ jacobi.c
@@ -24,7 +24,7 @@ int jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, doubl
             {
                 for (k = 1; k < N + 1; k++)
                 {
-                    u_upd[i][j][k] = s*(u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k]);
+                    u_upd[i][j][k] = s * (u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k]);
 
                     // Update the distance between u and u_upd
                     distance += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
@@ -46,7 +46,6 @@ int jacobi(double ***u, double ***u_upd, double ***f, int N, int iter_max, doubl
     return 0;
 }
 
-
 int jacobi_baseline(double ***u, double ***u_upd, double ***f, int N, int iter_max, double threshold)
 {
     int i, j, k, num_iter = 0, threads = 0;
@@ -60,35 +59,36 @@ int jacobi_baseline(double ***u, double ***u_upd, double ***f, int N, int iter_m
     {
         // Put the distance between u and u_upd to 0 now that the loop is started
         distance = 0;
-        // #pragma omp parallel private(i, j, k) shared(distance, num_iter)
-        #pragma omp parallel shared(distance)
+// #pragma omp parallel private(i, j, k) shared(distance, num_iter)
+#pragma omp parallel shared(distance)
         {
-          // threads = omp_get_num_threads();
-          #pragma omp for reduction(+: distance)
-          for (i = 1; i < N + 1; i++)
-          {
-              for (j = 1; j < N + 1; j++)
-              {
-                  for (k = 1; k < N + 1; k++)
-                  {
-                      u_upd[i][j][k] = s*(u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k]);
-                      // Update the distance between u and u_upd
-                      distance += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
-                  }
-              }
-          }
+// threads = omp_get_num_threads();
+#pragma omp for reduction(+ \
+                          : distance)
+            for (i = 1; i < N + 1; i++)
+            {
+                for (j = 1; j < N + 1; j++)
+                {
+                    for (k = 1; k < N + 1; k++)
+                    {
+                        u_upd[i][j][k] = s * (u[i - 1][j][k] + u[i + 1][j][k] + u[i][j - 1][k] + u[i][j + 1][k] + u[i][j][k - 1] + u[i][j][k + 1] + delta * delta * f[i][j][k]);
+                        // Update the distance between u and u_upd
+                        distance += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
+                    }
+                }
+            }
 
-        // Put u_upd into u to remake the calculations another time
-        
-        #pragma omp for
-          for (i = 1; i < N + 1; i++)
-              for (j = 1; j < N + 1; j++)
-                  for (k = 1; k < N + 1; k++)
-                  {
-                      u[i][j][k] = u_upd[i][j][k];
-                  }
-      }
-      num_iter += 1;
+            // Put u_upd into u to remake the calculations another time
+
+#pragma omp for
+            for (i = 1; i < N + 1; i++)
+                for (j = 1; j < N + 1; j++)
+                    for (k = 1; k < N + 1; k++)
+                    {
+                        u[i][j][k] = u_upd[i][j][k];
+                    }
+        }
+        num_iter += 1;
     }
     printf("%d %f %d %d ", N, distance, num_iter, threads);
     return threads;
@@ -103,32 +103,33 @@ int jacobi_barrier(double ***u, double ***u_upd, double ***f, int N, int iter_ma
 
     double s = 1.0 / 6.0;
 
-    #pragma omp parallel shared(distance, temp_dis, num_iter, threshold)
+#pragma omp parallel shared(distance, temp_dis, num_iter, threshold)
     while (distance > threshold && num_iter < iter_max)
     {
         // Put the distance between u and u_upd to 0 now that the loop is started
-      #pragma omp barrier
-      #pragma omp for reduction(+: temp_dis)
-      for (i = 1; i < N + 1; i++)
-      {
-          for (j = 1; j < N + 1; j++)
-          {
-              for (k = 1; k < N + 1; k++)
-              {
-                  u_upd[i][j][k] = s * (u[i - 1][j][k] + u[i + 1][j][k] +
-                                        u[i][j - 1][k] + u[i][j + 1][k] + 
-                                        u[i][j][k - 1] + u[i][j][k + 1] + 
-                                        delta * delta * f[i][j][k]);
+#pragma omp barrier
+#pragma omp for reduction(+ \
+                          : temp_dis)
+        for (i = 1; i < N + 1; i++)
+        {
+            for (j = 1; j < N + 1; j++)
+            {
+                for (k = 1; k < N + 1; k++)
+                {
+                    u_upd[i][j][k] = s * (u[i - 1][j][k] + u[i + 1][j][k] +
+                                          u[i][j - 1][k] + u[i][j + 1][k] +
+                                          u[i][j][k - 1] + u[i][j][k + 1] +
+                                          delta * delta * f[i][j][k]);
 
-                  // Update the distance between u and u_upd
-                  temp_dis += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
-              }
-          }
-      }
+                    // Update the distance between u and u_upd
+                    temp_dis += (u_upd[i][j][k] - u[i][j][k]) * (u_upd[i][j][k] - u[i][j][k]);
+                }
+            }
+        }
 
-        // Put u_upd into u to remake the calculations another time
-      	// #pragma omp parallel for collapse(3) private(i, j, k) reduction(+: distance) schedule(static)
-        #pragma omp for 
+// Put u_upd into u to remake the calculations another time
+// #pragma omp parallel for collapse(3) private(i, j, k) reduction(+: distance) schedule(static)
+#pragma omp for
         for (i = 1; i < N + 1; i++)
         {
             for (j = 1; j < N + 1; j++)
@@ -140,15 +141,13 @@ int jacobi_barrier(double ***u, double ***u_upd, double ***f, int N, int iter_ma
             }
         }
 
-        #pragma omp single
+#pragma omp single
         {
-          distance = temp_dis;
-          temp_dis = 0;
-          num_iter += 1;
+            distance = temp_dis;
+            temp_dis = 0;
+            num_iter += 1;
         }
-
-      }
+    }
     printf("%d %f %d %d ", N, distance, num_iter, threads);
     return threads;
 }
-
--- main.c
+++ main.c
@@ -7,7 +7,6 @@
 #include "print.h"
 #include <omp.h>
 
-
 #ifdef _JACOBI
 #include "jacobi.h"
 #endif
@@ -18,117 +17,132 @@
 
 #define N_DEFAULT 100
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
-    int 	N = N_DEFAULT, M;
-    int 	iter_max = 1000;
-    double	tolerance;
-    double	start_T;
-    int		output_type = 0;
-    char	*output_prefix = "poisson_res";
-    char        *output_ext    = "";
-    char	output_filename[FILENAME_MAX];
-    double 	***u = NULL, ***f = NULL, ***u_upd = NULL;
+    int N = N_DEFAULT, M;
+    int iter_max = 1000;
+    double tolerance;
+    double start_T;
+    int output_type = 0;
+    char *output_prefix = "poisson_res";
+    char *output_ext = "";
+    char output_filename[FILENAME_MAX];
+    double ***u = NULL, ***f = NULL, ***u_upd = NULL;
     int i, j, k;
 
-
     /* get the paramters from the command line */
-    N         = atoi(argv[1]);	// grid size
-    iter_max  = atoi(argv[2]);  // max. no. of iterations
-    tolerance = atof(argv[3]);  // tolerance
-    start_T   = atof(argv[4]);  // start T for all inner grid points
-    if (argc == 6) {
-	output_type = atoi(argv[5]);  // output type
+    N = atoi(argv[1]);         // grid size
+    iter_max = atoi(argv[2]);  // max. no. of iterations
+    tolerance = atof(argv[3]); // tolerance
+    start_T = atof(argv[4]);   // start T for all inner grid points
+    if (argc == 6)
+    {
+        output_type = atoi(argv[5]); // output type
     }
     M = N + 2;
     // allocate memory for u, u_upd and f
-    if ( (u = d_malloc_3d(M, M, M)) == NULL ) {
+    if ((u = d_malloc_3d(M, M, M)) == NULL)
+    {
         perror("array u: allocation failed");
         exit(-1);
     }
-    if ( (u_upd = d_malloc_3d(M, M, M)) == NULL ) {
+    if ((u_upd = d_malloc_3d(M, M, M)) == NULL)
+    {
         perror("array u_upd: allocation failed");
         exit(-1);
     }
-    if ( (f = d_malloc_3d(M, M, M)) == NULL ) {
+    if ((f = d_malloc_3d(M, M, M)) == NULL)
+    {
         perror("array f: allocation failed");
         exit(-1);
     }
-    
 
     //Set initial values for u
-    for(i = 0; i < M; i++){
-        for(j = 0; j < M; j++){
-            for(k = 0; k < M; k++){
-                if ((i == 0) || (i == M - 1) || (j == M - 1) || (k == 0) || (k == M - 1)){
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            for (k = 0; k < M; k++)
+            {
+                if ((i == 0) || (i == M - 1) || (j == M - 1) || (k == 0) || (k == M - 1))
+                {
                     u[i][j][k] = 20;
                 }
-                else {if (j == 0){
-                    u[i][j][k] = 0;
+                else
+                {
+                    if (j == 0)
+                    {
+                        u[i][j][k] = 0;
+                    }
+                    else
+                    {
+                        u[i][j][k] = start_T;
+                    }
                 }
-                else{
-                    u[i][j][k] = start_T;
-                }
-                }
-
             }
         }
     }
 
     //Set values for f
-    for(i = 0; i < M; i++){
-        for(j = 0; j < M; j++){
-            for(k = 0; k < M; k++){
-                if ((i <= (M-1) * 5 / 16) && (j <= (M-1) / 4) && (k <= (M-1) / 2) && (k >= (M-1) / 6)){
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < M; j++)
+        {
+            for (k = 0; k < M; k++)
+            {
+                if ((i <= (M - 1) * 5 / 16) && (j <= (M - 1) / 4) && (k <= (M - 1) / 2) && (k >= (M - 1) / 6))
+                {
                     f[i][j][k] = 200;
                 }
-                else{
+                else
+                {
                     f[i][j][k] = 0;
                 }
-
             }
         }
     }
 
     double stime, ftime, exec_time;
     int iteration = 0;
-    //Obtain solution u using the Jacobi method (not sure if N or M, depends on implementation by Jasmine)
-    #ifdef _JACOBI
+
+//Obtain solution u using the Jacobi method
+#ifdef _JACOBI
     stime = omp_get_wtime();
     // iteration = jacobi(u, u_upd, f, N, iter_max, tolerance);
     jacobi_baseline(u, u_upd, f, N, iter_max, tolerance);
     ftime = omp_get_wtime();
-    #endif
+#endif
 
-    #ifdef _GAUSS_SEIDEL
+#ifdef _GAUSS_SEIDEL
     stime = omp_get_wtime();
     iteration = gauss_seidel(u, u_upd, f, N, iter_max, tolerance);
     ftime = omp_get_wtime();
-    #endif
+#endif
 
     printf("%f\n", ftime - stime);
 
-    // dump  results if wanted 
-    switch(output_type) {
-	case 0:
-	    // no output at all
-	    break;
-	case 3:
-	    output_ext = ".bin";
-	    sprintf(output_filename, "%s_%d%s", output_prefix, N, output_ext);
-	    fprintf(stderr, "Write binary dump to %s: ", output_filename);
-	    print_binary(output_filename, N, u);
-	    break;
-	case 4:
-	    output_ext = ".vtk";
-	    sprintf(output_filename, "%s_%d%s", output_prefix, N, output_ext);
-	    fprintf(stderr, "Write VTK file to %s: ", output_filename);
-	    print_vtk(output_filename, M, u);
-	    break;
-	default:
-	    fprintf(stderr, "Non-supported output type!\n");
-	    break;
+    // dump  results if wanted
+    switch (output_type)
+    {
+    case 0:
+        // no output at all
+        break;
+    case 3:
+        output_ext = ".bin";
+        sprintf(output_filename, "%s_%d%s", output_prefix, N, output_ext);
+        fprintf(stderr, "Write binary dump to %s: ", output_filename);
+        print_binary(output_filename, N, u);
+        break;
+    case 4:
+        output_ext = ".vtk";
+        sprintf(output_filename, "%s_%d%s", output_prefix, N, output_ext);
+        fprintf(stderr, "Write VTK file to %s: ", output_filename);
+        print_vtk(output_filename, M, u);
+        break;
+    default:
+        fprintf(stderr, "Non-supported output type!\n");
+        break;
     }
 
     // de-allocate memory
@@ -136,5 +150,5 @@ main(int argc, char *argv[]) {
     free(u_upd);
     free(f);
 
-    return(0);
+    return (0);
 }
